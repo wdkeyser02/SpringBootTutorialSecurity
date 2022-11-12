75273e3a4a12e8d3cebe708fca1d37d348dcf6ed
willydekeyser <willy.de.keyser@skynet.be>
2022-11-12 15:45:18 +0100

    Setup HomeController

--- SpringBootSecurity/src/main/java/willydekeyser/controller/HomeController.java
+++ SpringBootSecurity/src/main/java/willydekeyser/controller/HomeController.java
@@ -1,5 +1,14 @@
 package willydekeyser.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HomeController {
 
+	
+	@GetMapping("/")
+	public String home() {
+		return "Welkom home!";
+	}
 }
