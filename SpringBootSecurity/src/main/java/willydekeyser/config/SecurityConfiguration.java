9f0779372cd1fe5f8a48e53d144837bc9f7abc39
willydekeyser <willy.de.keyser@skynet.be>
2022-11-25 17:01:49 +0100

    Setup JdbcUserDetailsManager

--- SpringBootSecurity/src/main/java/willydekeyser/config/SecurityConfiguration.java
+++ SpringBootSecurity/src/main/java/willydekeyser/config/SecurityConfiguration.java
@@ -1,46 +1,77 @@
 package willydekeyser.config;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
 
+
 	@Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
 			.authorizeHttpRequests(authConfig -> {
 				authConfig.requestMatchers(HttpMethod.GET, "/").permitAll();
+				authConfig.requestMatchers("/console/**").permitAll();
 				authConfig.requestMatchers(HttpMethod.GET, "/user").hasRole("USER");
 				authConfig.requestMatchers(HttpMethod.GET, "/admin").hasRole("ADMIN");
 				authConfig.anyRequest().authenticated();
 			})
+			.csrf().disable()
+			.cors().disable()
+			.headers().frameOptions().disable()
+			.and()
 			.formLogin(withDefaults()) // Login with browser and Build in Form
 			.httpBasic(withDefaults()); // Login with Insomnia or Postman and Basic Auth
 		return http.build();
 	}
 	
 	@Bean
-	UserDetailsService userDetailsService() {
-		var admin = User.builder()
-				.username("Willy De Keyser")
-				.password("{noop}password")
-				.roles("USER", "ADMIN")
-				.build();
-		var user = User.builder()
-				.username("Ken De Keyser")
-				.password("{noop}password")
-				.roles("USER")
-				.build();
-		return new InMemoryUserDetailsManager(admin, user);
+	UserDetailsManager users(DataSource dataSource) {
+		UserDetails user = User.builder()
+			.username("user")
+			.password("{noop}password")
+			.roles("USER")
+			.build();
+		UserDetails admin = User.builder()
+			.username("admin")
+			.password("{noop}password")
+			.roles("USER", "ADMIN")
+			.build();
+		JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
+		users.createUser(user);
+		users.createUser(admin);
+		return users;
 	}
+	
+	@Bean
+	DataSource dataSource() {
+		return new EmbeddedDatabaseBuilder()
+			.setType(EmbeddedDatabaseType.H2)
+			.addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
+			.build();
+	}
+	
+	//@Bean
+	//JdbcUserDetailsManager userDetailsService(DataSource dataSource) {
+	//	JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager();
+	//	jdbcUserDetailsManager.setDataSource(dataSource);
+	//	return jdbcUserDetailsManager;
+	//}
 }
