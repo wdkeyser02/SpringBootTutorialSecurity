91524e227216fce1ed91e01e96d2c95ebae3c303
willydekeyser <willy.de.keyser@skynet.be>
2022-11-27 10:42:15 +0100

    Update H2 console

--- SpringBootSecurity/src/main/java/willydekeyser/config/SecurityConfiguration.java
+++ SpringBootSecurity/src/main/java/willydekeyser/config/SecurityConfiguration.java
@@ -4,7 +4,6 @@ import static org.springframework.security.config.Customizer.withDefaults;
 
 import javax.sql.DataSource;
 
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -22,20 +21,20 @@ public class SecurityConfiguration {
 	@Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
-			.securityMatcher(PathRequest.toH2Console()) //H2 database
 			.authorizeHttpRequests(authConfig -> {
 				authConfig.requestMatchers(HttpMethod.GET, "/").permitAll();
 				authConfig.requestMatchers(HttpMethod.GET, "/user").hasRole("USER");
 				authConfig.requestMatchers(HttpMethod.GET, "/admin").hasRole("ADMIN");
 				authConfig.anyRequest().authenticated();
 			})
+			.csrf(csrf -> csrf.disable())
 			.headers().frameOptions().disable()
 			.and()
 			.formLogin(withDefaults()) // Login with browser and Build in Form
-			.httpBasic(withDefaults()); // Login with Insomnia or Postman and Basic Auth
+			.httpBasic(withDefaults()); // Login with Insomnia or Postman and Basic Auth       
 		return http.build();
 	}
-	
+		
 	@Bean
 	JdbcUserDetailsManager jdbcUserDetailsManager(DataSource dataSource) {
 		JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
@@ -43,7 +42,7 @@ public class SecurityConfiguration {
 	}
 	
 	@Bean
-    public DataSource getDataSource() {
+    DataSource getDataSource() {
 		return DataSourceBuilder.create()
 				.driverClassName("org.h2.Driver")
 				.url("jdbc:h2:mem:user")
