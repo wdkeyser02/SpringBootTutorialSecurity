b3bcc8ac42c72cb4e1952ba636ffb875e3dbb953
willydekeyser <willy.de.keyser@skynet.be>
2022-12-10 11:53:07 +0100

    Update UsersRepository

--- SpringBootSecurity/src/main/java/willydekeyser/repository/UsersRepository.java
+++ SpringBootSecurity/src/main/java/willydekeyser/repository/UsersRepository.java
@@ -5,11 +5,9 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import willydekeyser.model.Users;
 
-@Repository
 public interface UsersRepository extends JpaRepository<Users, Integer> {
 
 	@EntityGraph(type = EntityGraph.EntityGraphType.FETCH,
