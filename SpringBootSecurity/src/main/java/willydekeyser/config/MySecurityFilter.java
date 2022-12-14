c9bafbe98f5ad93820ae90f42492ec640dad8a34
willydekeyser <willy.de.keyser@skynet.be>
2022-12-14 15:54:20 +0100

    Update MySecurityFilter

--- SpringBootSecurity/src/main/java/willydekeyser/config/MySecurityFilter.java
+++ SpringBootSecurity/src/main/java/willydekeyser/config/MySecurityFilter.java
@@ -16,8 +16,11 @@ import jakarta.servlet.http.HttpServletResponse;
 
 public class MySecurityFilter extends OncePerRequestFilter {
 
-	private final String HEADER_LOGIN = "x-myfilter-login";
-	private final String HEADER_PASSWORD = "x-myfilter-password";
+	//private final String HEADER_LOGIN = "x-myfilter-login";
+	//private final String HEADER_PASSWORD = "x-myfilter-password";
+	
+	private final String PARAMETER_LOGIN = "login";
+	private final String PARAMETER_PASSWORD = "password";
 	
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
@@ -25,22 +28,42 @@ public class MySecurityFilter extends OncePerRequestFilter {
 		
 		System.out.println("\nBefore filterchain\n");
 		
-		if (!Collections.list(request.getHeaderNames()).contains(HEADER_LOGIN) || !Collections.list(request.getHeaderNames()).contains(HEADER_PASSWORD)) {
-			System.out.println("No Headers!");
+//		if (!Collections.list(request.getHeaderNames()).contains(HEADER_LOGIN) || !Collections.list(request.getHeaderNames()).contains(HEADER_PASSWORD)) {
+//			System.out.println("No Headers!");
+//			filterChain.doFilter(request, response);
+//			return;
+//		}
+	
+		
+		if (!Collections.list(request.getParameterNames()).contains(PARAMETER_LOGIN) || !Collections.list(request.getParameterNames()).contains(PARAMETER_PASSWORD)) {
+			System.out.println("No Parameters!");
 			filterChain.doFilter(request, response);
 			return;
 		}
 		
-		var login = request.getHeader(HEADER_LOGIN);
-		var password = request.getHeader(HEADER_PASSWORD);
+//		var login = request.getHeader(HEADER_LOGIN);
+//		var password = request.getHeader(HEADER_PASSWORD);
+//		if (!"Developer".equals(login) || !"password".equals(password)) {
+//			System.out.println("Headers: " + login + " - " + password);
+//			response.setStatus(HttpStatus.FORBIDDEN.value());
+//			response.getWriter().println("You are not the Developer!");
+//			return;
+//		}
+
+		var login = request.getParameter(PARAMETER_LOGIN);
+		var password = request.getParameter(PARAMETER_PASSWORD);
 		if (!"Developer".equals(login) || !"password".equals(password)) {
-			System.out.println("Headers: " + login + " - " + password);
+			System.out.println("PARAMETER: " + login + " - " + password);
 			response.setStatus(HttpStatus.FORBIDDEN.value());
 			response.getWriter().println("You are not the Developer!");
 			return;
 		}
+
+//		System.out.println("Headers: " + login + " - " + password);
+		System.out.println("Parameters: " + login + " - " + password);
 		
-		System.out.println("Headers: " + login + " - " + password);
+		
+		
 		var newContext = SecurityContextHolder.createEmptyContext();
 		UsernamePasswordAuthenticationToken authReq = new UsernamePasswordAuthenticationToken(login, null, AuthorityUtils.createAuthorityList("ROLE_USER", "ROLE_ADMIN", "ROLE_DEVELOPER"));
 		newContext.setAuthentication(authReq);
