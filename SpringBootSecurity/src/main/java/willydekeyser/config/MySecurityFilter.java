caafc5865691e06856296d2635479af878f4e88d
willydekeyser <willy.de.keyser@skynet.be>
2022-12-14 15:46:08 +0100

    Update MySecurityFilter

--- SpringBootSecurity/src/main/java/willydekeyser/config/MySecurityFilter.java
+++ SpringBootSecurity/src/main/java/willydekeyser/config/MySecurityFilter.java
@@ -1,7 +1,12 @@
 package willydekeyser.config;
 
 import java.io.IOException;
+import java.util.Collections;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import jakarta.servlet.FilterChain;
@@ -11,15 +16,38 @@ import jakarta.servlet.http.HttpServletResponse;
 
 public class MySecurityFilter extends OncePerRequestFilter {
 
+	private final String HEADER_LOGIN = "x-myfilter-login";
+	private final String HEADER_PASSWORD = "x-myfilter-password";
+	
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
 		
-		System.out.println("\n\nBefore filterchain ");
+		System.out.println("\nBefore filterchain\n");
 		
+		if (!Collections.list(request.getHeaderNames()).contains(HEADER_LOGIN) || !Collections.list(request.getHeaderNames()).contains(HEADER_PASSWORD)) {
+			System.out.println("No Headers!");
+			filterChain.doFilter(request, response);
+			return;
+		}
+		
+		var login = request.getHeader(HEADER_LOGIN);
+		var password = request.getHeader(HEADER_PASSWORD);
+		if (!"Developer".equals(login) || !"password".equals(password)) {
+			System.out.println("Headers: " + login + " - " + password);
+			response.setStatus(HttpStatus.FORBIDDEN.value());
+			response.getWriter().println("You are not the Developer!");
+			return;
+		}
+		
+		System.out.println("Headers: " + login + " - " + password);
+		var newContext = SecurityContextHolder.createEmptyContext();
+		UsernamePasswordAuthenticationToken authReq = new UsernamePasswordAuthenticationToken(login, null, AuthorityUtils.createAuthorityList("ROLE_USER", "ROLE_ADMIN", "ROLE_DEVELOPER"));
+		newContext.setAuthentication(authReq);
+		SecurityContextHolder.setContext(newContext);		
 		filterChain.doFilter(request, response);
 		
-		System.out.println("\n\nAfter filterchain ");
+		System.out.println("\nAfter filterchain\n");
 		
 	}
 
